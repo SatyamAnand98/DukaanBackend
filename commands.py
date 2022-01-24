f1ad306e3137b84b7bcd439d9b85241d333afb7e
Satyam Anand <thesatemail@gmail.com>
2022-01-24 17:44:44 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 import os
 
+os.system("pip install -r requirements.txt")
 os.system("python manage.py makemigrations dukaanBanao")
 os.system("python manage.py makemigrations")
 os.system("python manage.py migrate")
